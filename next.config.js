778192a9ad8145125d6fbc203475c204bc9bf5b7
shimmer-1 <92008218+shimmer-1@users.noreply.github.com>
2021-11-20 14:42:49 +0530

    cors fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/next-chat-app/' : ''
+  assetPrefix: isProd ? '/next-chat-app/' : '',
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
