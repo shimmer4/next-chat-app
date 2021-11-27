9809038910140f81a8f407a0f0e65ad6bb9dbf1f
shimmer-1 <92008218+shimmer-1@users.noreply.github.com>
2021-11-28 00:19:22 +0530

    vercel bgs 2

--- redux/chatSlice.js
+++ redux/chatSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-var initialState = []
+var initialState = [ {"name":"bot", "message": "welcome to next-chat-app"} ]
 
 export const chatSlice = createSlice( {
   name: 'login',
