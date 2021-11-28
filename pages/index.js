e0eb5312132152f20e35ec1e19a38e6f707f65cd
shimmer-1 <92008218+shimmer-1@users.noreply.github.com>
2021-11-28 21:33:44 +0530

    next js config bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const index = () => {
+
+    const router = useRouter()
+
+    React.useEffect( () => {
+        router.push('/login')
+    }, [] )
+
     return <p>Redirecting to login page...</p>
 }
 
