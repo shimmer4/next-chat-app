c16b2bd3b72cb2718c2820f4dc1cdfc73fa051b1
shimmer-1 <92008218+shimmer-1@users.noreply.github.com>
2021-11-29 17:55:36 +0530

    server addr change

--- components/socket.js
+++ components/socket.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { io } from 'socket.io-client'
 
-const socket = io.connect('https://first-flask-api.shimmer4.repl.co', {
+const socket = io.connect('https://flask-socket-server.shimmer4.repl.co', {
     reconnect: true,
     cors: { origin: "http://localhost:3000", credentials: true },
     transports: ['websocket']
