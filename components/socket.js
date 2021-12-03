944b7ffc3a3c514767485d626a84d42b1803897a
shimmer-1 <92008218+shimmer-1@users.noreply.github.com>
2021-12-03 14:14:28 +0530

    some websocket bugs

--- components/socket.js
+++ components/socket.js
@@ -1,10 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { io } from 'socket.io-client'
 
+// websockets transport return 400 response code
+// only polling working
+
+// need to install eventlet in server side to run websockets transport
+
 const socket = io.connect('https://flask-socket-server.shimmer4.repl.co', {
     reconnect: false,
     autoConnect: false,
-    cors: { origin: "http://localhost:3000", credentials: true },
+    cors: { origin: "*", credentials: true },
     transports: ['websocket']
     } )
 
